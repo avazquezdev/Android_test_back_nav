2bdcdda66c3f49764b64157571abe267ab30e2bc
AVazquez <avazquez.dev@gmail.com>
2022-08-12 08:43:04 +0200

    Android nav example

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btn = findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
